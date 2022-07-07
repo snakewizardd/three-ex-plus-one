bdc597df0d22d363adf5fb60bc8aaad7d614c0cc
snakewizardd <83378208+snakewizardd@users.noreply.github.com>
2022-07-06 23:51:20 -0400

    results

--- formulas.R
+++ formulas.R
@@ -5,6 +5,7 @@ library(ggplot2)
 library(esquisse)
 library(tidyr)
 library(rpivotTable)
+library(readr)
 
 #Create function to iterate 3x+1 and kill it when appropriately
 
@@ -48,6 +49,8 @@ calc <- function(number){
 #Constructing two matrices
 #1. the matrix to contain all the random values to try
 #2. The matrix to store the results of our operations
+
+#Starting with arbitrary x = 100, y= 100 parameters
 xmin=-2
 xmax=2
 nx=100
@@ -59,10 +62,11 @@ ny=100
 y <- seq(ymin, ymax, length.out=ny)
 
 
+#Constructing the random z matrix of test cases
 z <- matrix(ceiling(runif(length(x)*length(y))*1000000000), nrow=length(x), ncol=length(y))
-k <- matrix(0.0, nrow=length(x), ncol=length(y))
 
-####
+#The capture matrix
+k <- matrix(0.0, nrow=length(x), ncol=length(y))
 
 #Ensuring we are not considering any 1s by accident
 index <- which(z[,] != 1)
@@ -90,7 +94,7 @@ for(row in 1:dim(z)[1]){
   
 ########
 
-#Analyzing the top trends
+#Analyzing the top trends from the 10k observations
 top <- rows %>% group_by(k_value) %>%
     count() %>%
   arrange(-n) 
@@ -101,70 +105,4 @@ noteworthy <- rows %>% filter(k_value %in% top$k_value)
 
 #####
 
-#Running the analysis of top trends 100 times and rbinding
 
-repeat_this <- function(){
-  #Constructing two matrices
-  #1. the matrix to contain all the random values to try
-  #2. The matrix to store the results of our operations
-  xmin=-2
-  xmax=2
-  nx=100
-  x <- seq(xmin, xmax, length.out=nx)
-  
-  ymin=-1.5
-  ymax=1.5
-  ny=100
-  y <- seq(ymin, ymax, length.out=ny)
-  
-  
-  z <- matrix(ceiling(runif(length(x)*length(y))*1000000000), nrow=length(x), ncol=length(y))
-  k <- matrix(0.0, nrow=length(x), ncol=length(y))
-  
-  ####
-  
-  #Ensuring we are not considering any 1s by accident
-  index <- which(z[,] != 1)
-  
-  #Running the matrix-wise operation and outputting to k
-  for(index_number in index){
-    
-    k[index_number] <-  max(calc(z[index_number])$capture_iteration)
-    
-  }
-  
-  #Formatting the dataset into readable format
-  
-  rows <- NULL
-  
-  for(row in 1:dim(z)[1]){
-    
-    for(col in 1:dim(z)[2]){
-      
-      rows <- rbind(rows,data.frame(z_value = z[row,col], k_value = k[row,col]))
-      
-    }
-    
-  }
-  
-  ########
-  
-  #Analyzing the top trends
-  top <- rows %>% group_by(k_value) %>%
-    count() %>%
-    arrange(-n) 
-  
-  top <- top[1:15,]
-  
-  return(top)
-}
-
-capture_tops <- NULL
-
-for(i in 1:100){
-  
-  capture_tops <- rbind(capture_tops,repeat_this())
-  
-}
-  
-  
