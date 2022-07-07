7f99f565eb016e60aaa02c37acd501515265f95d
snakewizardd <83378208+snakewizardd@users.noreply.github.com>
2022-07-07 13:46:27 -0400

    analyzing 50

--- analyzing_50.R
+++ analyzing_50.R
@@ -22,3 +22,58 @@ for (number in fifty$z_value) {
 analysis <- analysis %>% filter(V1 != 'END') %>% select(-V1)
 
 colnames(analysis) <- c('NUMBER', 'EVEN', 'ODD')
+
+
+#####
+a <- calc(309019176)
+
+b <- calc(42915)
+
+
+nrow(a) == nrow(b)
+
+common_steps_b <- b[which(b$capture_number %in% a$capture_number),]
+common_steps_a <- a[which(b$capture_number %in% a$capture_number),]
+
+common_steps_a == common_steps_b
+
+uncommon_steps_b <- b[-which(b$capture_number %in% a$capture_number),]
+uncommon_steps_a <- a[-which(b$capture_number %in% a$capture_number),]
+
+#checking the even/odd distributions
+table(uncommon_steps_a$capture_state)
+table(uncommon_steps_b$capture_state)
+
+par(mfrow=c(1,2))
+
+plot(uncommon_steps_a$capture_iteration,factor(uncommon_steps_a$capture_state,
+                                               levels=c('ODD','EVEN')))
+plot(uncommon_steps_b$capture_iteration,factor(uncommon_steps_b$capture_state,
+                                               levels=c('ODD','EVEN')))
+
+#Factorizing
+
+uncommon_steps_a$to_factor <- factor(uncommon_steps_a$capture_state,
+                                     levels=c('ODD','EVEN')) %>% as.numeric() 
+
+uncommon_steps_a$to_factor <- ifelse(uncommon_steps_a$to_factor == 2,0,uncommon_steps_a$to_factor)
+
+uncommon_steps_b$to_factor <- factor(uncommon_steps_b$capture_state,
+                                     levels=c('ODD','EVEN')) %>% as.numeric()
+
+uncommon_steps_b$to_factor <- ifelse(uncommon_steps_b$to_factor == 2,0,uncommon_steps_b$to_factor)
+
+### Useful visualizations
+rpivotTable(data = uncommon_steps_a,
+            rows=c('capture_state'),
+            cols=c('capture_iteration'),
+            vals = c('capture_number'),
+            rendererName = 'Line Chart',
+            aggregatorName = 'Sum')
+
+rpivotTable(data = uncommon_steps_b,
+            rows=c('capture_state'),
+            cols=c('capture_iteration'),
+            vals = c('capture_number'),
+            rendererName = 'Line Chart',
+            aggregatorName = 'Sum')
