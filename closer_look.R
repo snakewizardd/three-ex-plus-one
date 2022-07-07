cf227681a0d1e47d4599597d0450a8474559ff6d
snakewizardd <83378208+snakewizardd@users.noreply.github.com>
2022-07-07 11:17:20 -0400

    even-odd-analysis

--- closer_look.R
+++ closer_look.R
@@ -19,7 +19,7 @@ for (number in two_seventeen$z_value) {
 
 }
 
-analysis <- analysis %>% filter(V1 != 'EVEN') %>% select(-V3)
+analysis <- analysis %>% filter(V1 != 'END') %>% select(-V1)
 
 colnames(analysis) <- c('NUMBER', 'EVEN', 'ODD')
 
@@ -47,7 +47,7 @@ for (number in one_ninety_nine$z_value) {
 
 }
 
-analysis <- analysis %>% filter(V1 != 'EVEN') %>% select(-V3)
+analysis <- analysis %>% filter(V1 != 'END') %>% select(-V1)
 
 colnames(analysis) <- c('NUMBER', 'EVEN', 'ODD')
 
@@ -74,8 +74,64 @@ for (number in one_eighty_six$z_value) {
 
 }
 
-analysis <- analysis %>% filter(V1 != 'EVEN') %>% select(-V3)
+analysis <- analysis %>% filter(V1 != 'END') %>% select(-V1)
 
 colnames(analysis) <- c('NUMBER', 'EVEN', 'ODD')
 
 write_csv(analysis, './output_files/analyzing_even_odd_trends_186_iterations.csv')
+
+#####
+
+one_sixty_eight <- import %>% filter(k_value %in% '168') %>%
+  mutate(length = nchar(z_value))
+
+
+analysis <- NULL
+
+for (number in one_sixty_eight$z_value) {
+  
+  data <- calc(number)
+  
+  table <- table(data$capture_state) %>% as.data.frame() %>% t() %>% as.data.frame() %>%
+    mutate(number_considered = number) %>% select(number_considered,everything())
+  
+  
+  analysis <- rbind(analysis,table)
+  
+  
+}
+
+analysis <- analysis %>% filter(V1 != 'END') %>% select(-V1)
+
+colnames(analysis) <- c('NUMBER', 'EVEN', 'ODD')
+
+write_csv(analysis, './output_files/analyzing_even_odd_trends_168_iterations.csv')
+
+###
+
+two_twelve <- import %>% filter(k_value %in% '212') %>%
+  mutate(length = nchar(z_value))
+
+
+analysis <- NULL
+
+for (number in two_twelve$z_value) {
+  
+  data <- calc(number)
+  
+  table <- table(data$capture_state) %>% as.data.frame() %>% t() %>% as.data.frame() %>%
+    mutate(number_considered = number) %>% select(number_considered,everything())
+  
+  
+  analysis <- rbind(analysis,table)
+  
+  
+}
+
+analysis <- analysis %>% filter(V1 != 'END') %>% select(-V1)
+
+colnames(analysis) <- c('NUMBER', 'EVEN', 'ODD')
+
+
+write_csv(analysis, './output_files/analyzing_even_odd_trends_212_iterations.csv')
+
